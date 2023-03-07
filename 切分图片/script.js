1363b1e7094b8951490ee0de2f368bf5271fc84c
lingmuyichen <3275790559@qq.com>
2023-03-07 18:39:35 +0800

    面向对象的笔记

--- 切分图片/script.js
+++ 切分图片/script.js
@@ -8,3 +8,5 @@ left.addEventListener('mouseleave', () => container.classList.remove('hover-left
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+/* 
+ */
